a965d131f09391ffb55219bc1e0fca8e53e8c10a
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2020-06-29 13:27:45 +0200

    Update HZ19_ELISA_CEWE.R

--- Mouse_eimeria_databasing_code/HZ19_ELISA_CEWE.R
+++ Mouse_eimeria_databasing_code/HZ19_ELISA_CEWE.R
@@ -37,7 +37,7 @@ HZ1<- merge(HZ1, HZ19_samples1)
 HZ1$OD <- NULL
 
 # write.csv(HZ1, "./Documents/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA1_complete.csv")
-write.csv(E1, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA1_complete.csv")
+write.csv(HZ1, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA1_complete.csv")
 
 ############### load in ELISA2
 
@@ -98,18 +98,80 @@ E3 <- dplyr::select(E3, IFNy)
 setDT(E3, keep.rownames = TRUE)[]
 colnames(E3)[1] <- "Mouse_ID"
 
-
+# write out ELISA 3
 write.csv(E3, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA3_complete.csv")
 
 
 
+
+
+############### load in ELISA4
+
+E4_std <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/ELISAs/HZ19_CEWE_ELISA4_std.csv"
+E4_std <- read.csv(text = getURL(E4_std))
+
+E4_samples <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/ELISAs/HZ19_CEWE_ELISA4_samples.csv"
+E4_samples <- read.csv(text = getURL(E4_samples))
+
+###### use drc to construct standard curve and pinpointprotein content
+
+model4<-drm(OD~Conc,
+            fct=LL.4(names=c("Slope", "Lower", "Upper", "ED50")),
+            data=E4_std)
+plot(model4)
+
+E4<-ED(model4, E4_samples$OD, type="absolute", display=F)
+row.names(E4) <- E4_samples$Mouse_ID
+
+points(y=E4_samples$OD,x=E4[,1],col="lightblue",pch=19,cex=2)
+text(y =E4_samples$OD, x = E4[,1], labels=E4_samples$Mouse_ID, data=E4, cex=0.9, font=2)
+
+E4 <- data.frame(E4)
+colnames(E4)[1] <- "IFNy"
+E4 <- dplyr::select(E4, IFNy)
+setDT(E4, keep.rownames = TRUE)[]
+colnames(E4)[1] <- "Mouse_ID"
+
+#write out ELISA 4
+write.csv(E4, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA4_complete.csv")
+
+
+############## Load in ELISA 5
+E5_std <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/ELISAs/HZ19_CEWE_ELISA5_std.csv"
+E5_std <- read.csv(text = getURL(E5_std))
+
+E5_samples <- "https://raw.githubusercontent.com/derele/Mouse_Eimeria_Databasing/master/data/ELISAs/HZ19_CEWE_ELISA5_samples.csv"
+E5_samples <- read.csv(text = getURL(E5_samples))
+
+###### use drc to construct standard curve and pinpointprotein content
+
+model5<-drm(OD~Conc,
+            fct=LL.4(names=c("Slope", "Lower", "Upper", "ED50")),
+            data=E5_std)
+plot(model5)
+
+E5<-ED(model5, E5_samples$OD, type="absolute", display=F)
+row.names(E5) <- E5_samples$Mouse_ID
+
+points(y=E5_samples$OD,x=E5[,1],col="lightblue",pch=19,cex=2)
+text(y =E5_samples$OD, x = E5[,1], labels=E5_samples$Mouse_ID, data=E5, cex=0.9, font=2)
+
+E5 <- data.frame(E5)
+colnames(E5)[1] <- "IFNy"
+E5 <- dplyr::select(E5, IFNy)
+setDT(E5, keep.rownames = TRUE)[]
+colnames(E5)[1] <- "Mouse_ID"
+
+#write out ELISA 4
+write.csv(E5, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA5_complete.csv")
+
 ######################### merge all and write out
-
-
-
 E <- rbind(HZ1, E2)
 E <- rbind(E, E3)
+E <- rbind(E, E4)
+E <- rbind(E, E5)
 
-write.csv(E, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISAs_complete.csv")
+
+write.csv(E, "C:/Users/Luke Bednar/Mouse_Eimeria_Databasing/data/ELISAs/HZ19_CEWE_ELISA.csv")
 
 #################
