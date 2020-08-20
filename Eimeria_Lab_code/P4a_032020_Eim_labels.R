2b3cf3004caca312c6797d762e58a5f59b0d4163
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2020-08-20 12:20:55 +0200

    Update P4a_032020_Eim_labels.R

--- Eimeria_Lab_code/P4a_032020_Eim_labels.R
+++ Eimeria_Lab_code/P4a_032020_Eim_labels.R
@@ -1,6 +1,25 @@
-# make design tables
-P4a <- read.csv("~/Eimeria_Lab/data/2_designTables/P4a_012020_Eim_design.csv")
+library(Rmisc)
+library(httr)
+library(RCurl)
+library(arsenal)
 
-P4a$labels <- sample(combn(LETTERS, 3, paste, collapse = ""), nrow(P4a))
+# read in record tables
+P4a <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4a_082020_Eim_Record.csv"))
+P4b <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4b_082020_Eim_Record.csv"))
 
-write.csv(P4a, "~/Eimeria_Lab/data/2_designTables/P4a_012020_Eim_design.csv")
+#make random labels
+random_labels <- do.call(paste0, replicate(3, sample(LETTERS, 480, TRUE), FALSE))
+RLa <- random_labels[1:240]
+RLb <- random_labels[241:480]
+
+#assign to label columns
+P4a$labels <- RLa
+P4b$labels <- RLb
+
+#check if labels are unique
+comparedf(P4a, P4b)
+summary(comparedf(P4a, P4b))
+
+#write out
+write.csv(P4a, "../Eimeria_Lab/data/Experiment_results/P4a_082020_Eim_Record.csv")
+write.csv(P4b, "../Eimeria_Lab/data/Experiment_results/P4b_082020_Eim_Record.csv")
