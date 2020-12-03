a4324729c549217a31b2710fe4c10c59eb399f5e
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2020-12-03 20:25:50 +0100

    P4 update to split record + oocyst

--- Eimeria_Lab_code/P4_082020_Eim_combine.R
+++ Eimeria_Lab_code/P4_082020_Eim_combine.R
@@ -12,14 +12,15 @@ P4a_design <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/E
 P4a_oocyst <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4a_082020_Eim_oocysts1.csv"))
 # add and clean record P4a
 P4a_record <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4a_082020_Eim_Record.csv"))
-P4a_record$wloss <- as.numeric(as.character(P4a_record$wloss))
+P4a_record$relative_weight <- as.numeric(as.character(P4a_record$relative_weight))
+P4a_record$relative_weight[which(P4a_record$relative_weight == "#VALUE!")] <- NA
 # add design P4b
 P4b_design <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/P4b_082020_Eim_design.csv"))
 # add and clean oocyst P4b
 P4b_oocyst <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4b_082020_Eim_oocysts.csv"))
 # add and clean record P4b
 P4b_record <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4b_082020_Eim_Record.csv"))
-P4b_record$wloss <- as.numeric(as.character(P4b_record$wloss))
+P4b_record$relative_weight <- as.numeric(as.character(P4b_record$relative_weight))
 # add cleaned qPCR P4b
 P4b_qPCR <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experiment_results/P4_082020_Eim_CEWE_qPCR.csv"))
 P4b_qPCR$dpi <- 8
@@ -63,34 +64,30 @@ P4b <- merge(P4b, P4b_ELISA, all = T)
 P4 <- merge(P4a, P4b, all = T)
 
 # calculate oocyst AVG, total and OPG
-P4$totalOocysts <- ((P4$oocyst_1 
-                          + P4$oocyst_2 
-                          + P4$oocyst_3 
-                          + P4$oocyst_4) / 4) * 
+P4$total_oocysts <- ((P4$oocyst_sq1 
+                          + P4$oocyst_sq2 
+                          + P4$oocyst_sq3 
+                          + P4$oocyst_sq4) / 4) * 
   10000 * # because volume chamber
   2
-P4$OPG <- P4$totalOocysts/ P4$faeces_weight
+P4$OPG <- P4$total_oocysts/ P4$feces_weight
 
 # replace !VALUE# with NAs
-P4$wloss[P4$wloss == "#VALUE!"] <- NA
 P4$day_change[P4$day_change == "#VALUE!"] <- NA
 # final clean to have same columns as P3:
 # label	EH_ID	IFNy_CEWE	delta	CXCR3	IL.12	IRG6	Eim_MC	AVG	totalOocysts	dpi	weight	
 # weight_dpi0	faeces_weight	wloss	batch	primary	challenge	infHistory	OPG	IFNy_FEC
 
-colnames(P4)[1] <- "label"
-P4$oocyst_1 <- NULL
-P4$oocyst_2 <- NULL
-P4$oocyst_3 <- NULL
-P4$oocyst_4 <- NULL
-P4$AVG <- NULL
 P4$day_change <- NULL
-P4[29] <- NULL
+P4$experiment <- "P4"
+P4$dilution <- 1
+
 
 write.csv(P4, "~/GitHub/Eimeria_Lab/data/Experiment_results/P4_082020_Eim_COMPLETE.csv")
-
-
-
+P4o <- select(P4, labels, experiment, oocyst_sq1, oocyst_sq2, oocyst_sq3, oocyst_sq4, oocyst_mean, OPG, dilution)
+write.csv(P4o, "~/GitHub/Eimeria_Lab/data/Experiment_results/P4_082020_Eim_oocyst.csv")
+P4w <- select(P4, labels, experiment, weight, weight_dpi0, relative_weight, feces_weight, dpi)
+write.csv(P4w, "~/GitHub/Eimeria_Lab/data/Experiment_results/P4_082020_Eim_record.csv")
 
 ggplot(subset(P4, !is.na(P4$Eim_MC)), aes(x = wloss, y = delta, color = Eim_MC)) +
   geom_jitter() +
