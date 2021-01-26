315c5b53cf01dc683db7e01b8a2632212e37b87a
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2021-01-26 17:33:18 +0100

    Update create_design_table.R

--- create_design_table.R
+++ create_design_table.R
@@ -1,16 +1,18 @@
 # create design table
 # Load information table
 library(RCurl)
+library(httr)
+
 # load in initial dataset from GitHub (must be raw.)
-infoTable = read.csv(text = getURL("desired info dataset"))
+infoTable <- read.csv(text = getURL("https://raw.githubusercontent.com/derele/Eimeria_Lab/master/data/Experimental_design/E11_012021_Eim_info.csv"))
 # check last experiment and get highest EH_ID
 lastEH_ID <- "last used EH_ID"
 # divide dataset into groups as desired
 ## e.g.: 100 mice would divide into c(rep("isolate1", 25), rep("isolate2", 25), 
 ##                                  rep("isolate3", 25), rep("uninfected", 25))
 # for challenge infection plans this has to be spread further
-primary_infection <- c(rep("E64", 27), rep("E88", 27))
-challenge_infection <- c(rep("E64", 27), rep("E88", 27))
+infection_history <- c(rep("UNI:UNI", 3), rep("E88:UNI", 3), rep("E64:UNI", 3), rep("UNI:E64", 3), rep("UNI:E88", 3), 
+                       rep("E88:E88", 4), rep("E64:E64", 4), rep("E88:E64", 4), rep("E64:E88", 4))
 # number of mice
 Nmice = nrow(infoTable)
 #Give EH_IDs
