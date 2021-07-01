6b27b83db01d76298e3483ae1f5ed0136ff7c045
Lubomir Bednar <lubomir.luke.bednar@gmail.com>
2021-07-01 18:04:28 +0200

    ngatives IFC + qPCR script for E10+E11

---
+++ Eimeria_Lab_code/E10_E11_CEWE_Eim_qPCR.R
@@ -0,0 +1,34 @@
+# E10 and E11 qPCRs
+library(dplyr)
+library(reshape2)
+library(tidyr)
+
+a <- read.csv("./E10_E11_qPCR_temp/E10_E11_CEWE_qPCR_01_repeat.csv")
+b <- read.csv("./E10_E11_qPCR_temp/E10_E11_CEWE_qPCR_02_Franzi.csv")
+b$Sample <- paste0("LM0", b$Sample)
+c <- read.csv("./E10_E11_qPCR_temp/E10_E11_Eim_CEWE_qPCR.csv")
+d <- read.csv("./E10_E11_qPCR_temp/E10_E11_qPCR_CEWE_01_01.csv")
+
+DF <- bind_rows(a, b, c, d)
+DF <- select(DF, Sample, Cq, Target)
+DF$Target[DF$Target == "Mus"] <- "mouse"
+DF$Target[DF$Target == "CDC42"] <- "mouse"
+DF$Target[DF$Target == "Eim"] <- "eimeria"
+DF$Target[DF$Target == "COI"] <- "eimeria"
+DF <- subset(DF, !is.na(DF$Cq))
+
+DF <- DF %>% group_by(Sample, Target) %>% summarise(across(everything(), list(mean)))
+DF <- pivot_wider(data = DF, names_from = Target, values_from = Cq_1)
+DF$delta <- DF$mouse - DF$eimeria
+
+design1 <- read.csv("./GitHub/Eimeria_Lab/data/Experimental_design/E10_112020_Eim_DESIGN.csv")
+design1$birthday <- NA
+design1$EH_ID <- gsub("_", "", design1$EH_ID)
+design2 <- read.csv("./GitHub/Eimeria_Lab/data/Experimental_design/E11_012021_Eim_DESIGN.csv")
+
+design <- bind_rows(design1, design2)
+names(DF)[names(DF) == "Sample"] <- "EH_ID"
+
+DF1 <- merge(design, DF, all.x = T)
+
+missing <- subset(DF1, is.na(DF1$delta))
